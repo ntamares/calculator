c840eb3b74fa209e060702cdc4ca68c824553921
ntamares <ntamares@gmail.com>
2023-02-17 22:58:33 -0600

    clean ui, add skeleton/psedocode
    ui still ugly, but bearable to look at now.

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 let query;
+const numberBtns = document.getElementsByClassName("number");
+const operandBtns = document.getElementsByClassName("operand");
+const equalsBtn = document.getElementById("btn-equals");
+const clearBtn = document.getElementById("btn-clear");
+const decimalBtn = document.getElementById("btn-decimal");
 
-function appendQuery(button){
-  let result; //use FIFO?
+// unnecessary?
+// function appendQuery(button){
+//   let result; 
 
-  return result;
+//   // use stack?
+//   // always display query and update result?
+//   // only after eqauls?
+
+//   return result;
+// }
+
+function appendOperand(operand){
+  const btnId = "btn-" + operand;
+  const btn = document.getElementById(btnId);
+
+  //check if numbers are first
+  // if operand is first on stack, ignore
+  // if after number, put operand next on stack
+  // if after number, there is already an operand, replace it
 }
+
+function appendNumber(num){
+  // get num by target.id
+  // check last entry into query
+  // if number append (as string then parse?)
+  // if after operand create next number on stack
+
+  // update and use getresult everyime?
+}
+
+function getResult(query){
+    let result;
+}
+
+// add listeners to each button
